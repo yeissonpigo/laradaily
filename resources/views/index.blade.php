1b19996ee5d533ebe083efd097b158630bd9db5a
Yeissonpigo <yeissonpigo@unisabana.edu.co>
2021-08-30 22:42:31 -0500

    the text in index area has been updated with a better design.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,11 +16,11 @@
         </div>
     </div>
     <div class="content__this">
-        ¿Qué es Laradaily?
+        <strong style="color: var(--orange-color); font-size: 1rem">#</strong>¿Qué es Laradaily?
         <p>Laradaily es un blog donde cualquier persona podrá publicar entradas, y compartir su conocimiento. Los temas principales de Laradaily son vida personal, y desarrollo. Si bien, Laradaily se enfoca en Laravel, y PHP, también se podrán ver publicaciones sobre Javascript, Python, java, etc.</p>
     </div>
     <div class="content__this">
-        ¿Por qué formar parte del equipo?
+        <strong style="color: var(--orange-color); font-size: 1rem">#</strong>¿Por qué formar parte del equipo?
         <p>Si te postulas a colaborador de Laradaily, tendrás la oportunidad de poner a prueba conocimientos básicos requeridos en cualquier puesto de desarrollador: HTML, CSS, Javascript, PHP, Laravel, bases de datos, git, github, bash, entre otros. Además, al tener acceso al repositorio, podrás poner este proyecto como parte de tu experiencia freelance en tu hoja de vida.</p>
     </div>
 @endsection
